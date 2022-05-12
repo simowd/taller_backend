2b6308937f058359cbef75a54a246aacd529adea
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-05-12 18:55:00 -0400

    Fixed how redis databases workds

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,13 +1,22 @@
 import Redis, { RedisOptions } from 'ioredis';
 import { NODE_ENV, REDIS_HOST } from './config';
 
-//Setup Redis Settings
-const redisSettings: RedisOptions = {
-  db: NODE_ENV === 'dev' ? 1 : 0,
+//Setup Redis Settings for the hashes
+const hashSettings: RedisOptions = {
+  db: NODE_ENV === 'dev' ? 2 : 0,
   port: 6379,
   host: REDIS_HOST
 };
 
-const redis = new Redis(redisSettings);
+//Setup Redis Settings for the streams
+const streamSettings: RedisOptions = {
+  db: NODE_ENV === 'dev' ? 3 : 1,
+  port: 6379,
+  host: REDIS_HOST
+};
 
-export default redis;
+//Create instance for different database entry point
+const hash = new Redis(hashSettings);
+const stream = new Redis(streamSettings);
+
+export { hash, stream };
