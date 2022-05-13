ae1d3e6070a9814cf6ef3ea10f7f0e757c7ca35d
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-05-12 22:49:28 -0400

    Refactored Data Handler on Socket Server

--- src/sockets/savingData.ts
+++ src/sockets/savingData.ts
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { Socket } from 'socket.io';
 import File from '../models/File';
 import { FileSocketBody } from '../types/file';
-import blobServiceClient from '../utils/azure_blob';
+import { blobFileUploader } from '../utils/azure_blob';
 import { socketLogger } from '../utils/logger';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -31,11 +31,7 @@ const saveAll = async (redisHash: Record<string, string>, socket: Socket) => {
 
     for (const file of data) {
       //Uploading Blob to Azure
-      const containerClient = blobServiceClient.getContainerClient(file.folder_storage);
-
-      const blockBlobClient = containerClient.getBlockBlobClient(file.file_storage);
-
-      await blockBlobClient.upload(file.value, file.value.length, { blobHTTPHeaders: { blobContentType: 'text/x-python' } });
+      await blobFileUploader(file.folder_storage, file.file_storage, file.value);
 
       await File.update({ ...transactionBuilder(socket) }, { where: { storage: file.file_storage } });
     }
@@ -50,11 +46,7 @@ const saveAll = async (redisHash: Record<string, string>, socket: Socket) => {
 const saveOne = async (data: FileSocketBody, socket: Socket) => {
   try {
     //Uploading Blob to Azure
-    const containerClient = blobServiceClient.getContainerClient(data.folder_storage);
-
-    const blockBlobClient = containerClient.getBlockBlobClient(data.file_storage);
-
-    await blockBlobClient.upload(data.value, data.value.length, { blobHTTPHeaders: { blobContentType: 'text/x-python' } });
+    await blobFileUploader(data.folder_storage, data.file_storage, data.value);
 
     //Update on database
     await File.update( {...transactionBuilder(socket)}, { where: { storage: data.file_storage }});
